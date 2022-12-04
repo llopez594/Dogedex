ba279f84730818f2a99e07b6c97ed9aac4df285f
llopez594 <cybernetweetco@gmail.com>
2022-12-03 21:21:16 -0400

    Manejando errores al descargar perros de internet

--- app/src/main/java/com/ljlopezm/dogedex/doglist/DogListViewModel.kt
+++ app/src/main/java/com/ljlopezm/dogedex/doglist/DogListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ljlopezm.dogedex.Dog
+import com.ljlopezm.dogedex.api.ApiResponseStatus
 import kotlinx.coroutines.launch
 
 class DogListViewModel : ViewModel() {
@@ -13,6 +14,10 @@ class DogListViewModel : ViewModel() {
     val dogList: LiveData<List<Dog>>
         get() = _dogList
 
+    private val _status = MutableLiveData<ApiResponseStatus>()
+    val status: LiveData<ApiResponseStatus>
+        get() = _status
+
     private val dogRepository = DogRepository()
 
     init {
@@ -20,7 +25,13 @@ class DogListViewModel : ViewModel() {
     }
     private fun downloadDogs() {
         viewModelScope.launch {
-            _dogList.value = dogRepository.donloadDogs()
+            _status.value = ApiResponseStatus.LOADING
+            try {
+                _dogList.value = dogRepository.donloadDogs()
+                _status.value = ApiResponseStatus.SUCCESS
+            } catch (e: Exception) {
+                _status.value = ApiResponseStatus.ERROR
+            }
         }
     }
 }
