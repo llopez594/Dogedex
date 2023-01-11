d1e57ab1cde1b2dfd3fa82e6fc92b49a900bf7a5
llopez594 <cybernetweetco@gmail.com>
2023-01-10 23:33:38 -0400

    Navegando entre auth fragments

--- app/src/main/java/com/ljlopezm/dogedex/auth/LoginActivity.kt
+++ app/src/main/java/com/ljlopezm/dogedex/auth/LoginActivity.kt
@@ -2,11 +2,17 @@ package com.ljlopezm.dogedex.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.ljlopezm.dogedex.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onRegisterButtonClick() {
+        findNavController(R.id.nav_host_fragment)
+            .navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
     }
 }
